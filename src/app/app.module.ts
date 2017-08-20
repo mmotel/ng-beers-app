85adfce285f507f6b10d22fac8968d412af31db6
Mateusz Motel <mateuszmotel@gmail.com>
2017-08-20 15:19:01 +0200

    added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdButtonModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
